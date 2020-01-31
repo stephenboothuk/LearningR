89cff3f5ae5c2b2b0556f8c817fd51d8bdb8fd96
Stephen Booth <stephenbooth.uk@gmail.com>
2020-01-31 17:53:20 +0000

    a

---
+++ chp01.R
@@ -0,0 +1,30 @@
+#multiply two times 3
+2*3
+x <- 2*3
+x
+x/2
+ls()
+x <- c(2.3, 1, 5)
+mode(x)
+class(x)
+length(x)
+x
+mynums <- 10:1
+mynums
+ls()
+sum(mynums)
+min(mynums)
+max(mynums)
+range(mynums)
+diff(range(mynums))
+mean(mynums)
+median(mynums)
+sd(mynums)
+var(mynums)
+mynums -5
+mynums*2
+mynums/2
+mynums[1]
+mynums[2]
+mynums[1:4]
+mynums[-2]
